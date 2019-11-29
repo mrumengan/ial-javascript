75d3cb89bcb1e2e2af11afdcb5f023319036f7d7
Legion\Mason Rumengan <mrumengan@taptalents.com>
2019-11-29 10:05:45 +0700

    [update]

--- routes/gawais.js
+++ routes/gawais.js
@@ -1,18 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var MongoClient = require('mongodb').MongoClient
 
 exports.list = function(req, res){
 
-  MongoClient.connect('mongodb://localhost:27017',
+  MongoClient.connect('mongodb://'+process.env.MONGODB_HOST+':'+process.env.MONGODB_PORT,
   {useUnifiedTopology: true, useNewUrlParser: true},
   function (err, client) {
     if (err) throw err
   
-    var db = client.db('meteor');
+    var db = client.db(process.env.MONGODB_DB);
     db.collection('gawai').find().limit(25).toArray(function (err, result) {
       if (err) throw err
   
-      console.log(result);
-
       res.render('gawais/index', { title: 'Gawai List', gawais: result });
     })
   });
